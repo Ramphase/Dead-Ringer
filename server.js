e7b3045492deb6e1cdbab3fc8eecf38fdb8379ab
Gavin <97692441+Ramphase@users.noreply.github.com>
2022-03-10 01:31:00 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -30,6 +30,16 @@ app.use((req, res, next) =>
   next();
 });
 
+if (process.env.NODE_ENV === 'production') 
+  {
+    // Set static folder
+    app.use(express.static('frontend/build'));
+    app.get('*', (req, res) => 
+  {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+  }
+
 app.post('/api/login', async (req, res, next) => 
 {
   // Incoming: login, password
@@ -52,6 +62,8 @@ app.post('/api/login', async (req, res, next) =>
   res.status(200).json(ret);
 
 });
+
+
 
 app.listen(PORT, () => 
 {
