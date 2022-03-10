42e6a380a716f7fc77da31a17809ad0b99e0a7a4
Gavin <97692441+Ramphase@users.noreply.github.com>
2022-03-10 00:06:48 -0500

    Add files via upload

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,1 +1,24 @@
+import React, { useState } from 'react';
 
+
+
+function Login()
+{
+    const app_name = 'dead-ringer'
+    function buildPath(route)
+    {
+        if (process.env.NODE_ENV === 'production') 
+        {
+            return 'https://' + app_name +  '.herokuapp.com/' + route;
+        }
+        else
+        {        
+            return 'http://localhost:5000/' + route;
+        }
+    }
+
+    return(
+        <h1>Dead Ringer is underconstruction</h1>
+    );
+};
+export default Login;
