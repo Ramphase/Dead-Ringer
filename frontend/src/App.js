fd34520b85ff012e8c5cb97193011d5851f09bfb
Gavin <97692441+Ramphase@users.noreply.github.com>
2022-03-10 00:05:57 -0500

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import './App.css';
+import LoginPage from './pages/LoginPage';
+
+function App() {
+  return (
+    <Router >
+      <Switch>
+        <Route path="/" exact>
+          <LoginPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
