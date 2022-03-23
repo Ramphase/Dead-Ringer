cfa6e570d6b653877b6e1edd3b05a7aac31a6978
Santi <35246905+SantiMelo@users.noreply.github.com>
2022-03-23 17:07:31 -0400

    login and register testing

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,23 +1,13 @@
-import React,{ useRef, useState, useEffect } from 'react';
+import React,{ useState } from 'react';
 import {Link} from 'react-router-dom';
 
 export function Login(){
-    const errRef = useRef();
-
-    const [user, setUser] = useState('');
-    const [pwd, setPwd] = useState('');
-    const [errMsg, setErrMsg] = useState('');
-    const [message, setMessage] = useState('');
-    const [success, setSuccess] = useState(false);
-
+   
     var loginName;
     var loginPassword;
-  
-    useEffect(() => {
-        setErrMsg('');
-    }, [user, pwd])
+    const [message, setMessage] = useState('');
 
-    const handleSubmit = async (e) => {
+    const doLogin = async (e) => {
         e.preventDefault();
 
         var obj = {login:loginName.value,password:loginPassword.value};
@@ -26,7 +16,7 @@ export function Login(){
 
         try
         {            
-            const response = await fetch(bp.buildPath('api/login'),
+            const response = await fetch(bp.buildPath('/api/login'),
                 {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
 
             var res = JSON.parse(await response.text());
@@ -52,66 +42,28 @@ export function Login(){
     }
 
     return (
-        <>
-            {success ? (
-                <section>
-                    <h1>You are logged in!</h1>
-                    <br />
+            <section>
+               <h2 className="mb-5">Login</h2>
+                   <form onSubmit={doLogin}>
+                    <input type="text" id="loginName" placeholder="Username" ref={(c) => loginName = c} className="mb-3"/> 
+                    <input type="password" id="password" placeholder="Password" ref={(c) => loginPassword = c} />
+                    <button className="mt-3" onSubmit={doLogin}>Sign In</button>
+                   </form>
                     <p>
-                        <a href="#">Go to Home</a>
-                    </p>
-                </section>
-            ) : (
-                <section>
-                    <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-                    <h2 className="mb-5">Login</h2>
-                    <form onSubmit={handleSubmit}>
-                   
-                        <input
-                            type="text"
-                            id="loginName"
-                            placeholder="Username"
-                            ref={(c) => loginName = c}
-                            autoComplete="off"
-                            onChange={(e) => setUser(e.target.value)}
-                            value={user}
-                            required
-                            className="mb-3"
-                        />
-
-                      
-                        <input
-                            type="password"
-                            id="password"
-                            placeholder="Password"
-                            ref={(c) => loginPassword = c}
-                            onChange={(e) => setPwd(e.target.value)}
-                            value={pwd}
-                            required
-                        />
-                        
-                        <button className="mt-3" onSubmit={handleSubmit}>Sign In</button>
-                    </form>
-                    <p>
-                    <span className="link text-center">
-                        <Link to="/Register" variant = "body2">
+                        <span className="link text-center">
+                            <Link to="/Register" variant = "body2">
                         <span style={{color :"black", fontSize: 15}}>
                              Don't have an account?
-                        </span>
-                        
+                        </span>    
                         <span span style={{color :'#4e4187', fontSize: 15}}>
                              Sign up
-                         </span>
-                        </Link>
                         </span>
-
+                            </Link>
+                        </span>
                         <span id="loginResult">{message}</span>
                     </p>
-                       
                 </section>
             )}
-        </>
-    )
-}
+
 
 export default Login
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,29 +1,9 @@
-import React,{ useRef, useState, useEffect } from "react";
-import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React,{ useState } from "react";
 import {Link} from 'react-router-dom';
 
-const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
-const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
-
 export function Register() {
-    const errRef = useRef();
-
-    const [user, setUser] = useState('');
-    const [validName, setValidName] = useState(false);
-    const [userFocus, setUserFocus] = useState(false);
-
-    const [pwd, setPwd] = useState('');
-    const [validPwd, setValidPwd] = useState(false);
-    const [pwdFocus, setPwdFocus] = useState(false);
-
-    const [matchPwd, setMatchPwd] = useState('');
-    const [validMatch, setValidMatch] = useState(false);
-    const [matchFocus, setMatchFocus] = useState(false);
-
-    const [errMsg, setErrMsg] = useState('');
+    
     const [message, setMessage] = useState('');
-    const [success, setSuccess] = useState(false);
 
     var registerLogin;
     var registerPassword;
@@ -36,20 +16,7 @@ export function Register() {
         '(.+)@((.+){2,})((.+){2,})'
     );
 
-    useEffect(() => {
-        setValidName(USER_REGEX.test(user));
-    }, [user])
-
-    useEffect(() => {
-        setValidPwd(PWD_REGEX.test(pwd));
-        setValidMatch(pwd === matchPwd);
-    }, [pwd, matchPwd])
-
-    useEffect(() => {
-        setErrMsg('');
-    }, [user, pwd, matchPwd])
-
-    const handleSubmit = async (e) => {
+    const doRegister = async (e) => {
         e.preventDefault();
 
         var obj = 
@@ -90,7 +57,7 @@ export function Register() {
 
         try
         {            
-            const response = await fetch(bp.buildPath('api/register'),
+            const response = await fetch(bp.buildPath('/api/register'),
                 {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
 
             var res = JSON.parse(await response.text());
@@ -104,7 +71,7 @@ export function Register() {
             var user = {firstName:res.firstName,lastName:res.lastName,id:res.id}
             localStorage.setItem('user_data', JSON.stringify(user));
 
-            setMessage('');
+            setMessage('Account Created');
             window.location.href = '/';
 
         }
@@ -116,112 +83,24 @@ export function Register() {
     }
 
     return (
-        <>
-            {success ? (
-                <section>
-                    <h1>Success!</h1>
-                    <p>
-                        <a href="#">Sign In</a>
-                    </p>
-                </section>
-            ) : (
-                <section >
-                    <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-                    <h2 className="mb-3">Register</h2>
-                    <form onSubmit={handleSubmit}>
-                   <div className="side-by-side">
-                  
-                   <input 
-                       type="text" 
-                       id="firstName" 
-                       placeholder="First Name"
-                       className="mb-3"
-                       ref={(c) => firstName = c}/>
-                    
-                     <input
-                       type="text"  
-                       id="lastName"  
-                       placeholder="Last Name"
-                       className="mb-3"
-                       ref={(c) => lastName = c}/>
-                   
-                   </div>
+
+          <section >
+            <h2 className="mb-3">Register</h2>
+                <form onSubmit={doRegister}>
+                    <div className="side-by-side">
+                        <input type="text" id="firstName" placeholder="First Name" className="mb-3" ref={(c) => firstName = c}/>
+                        <input type="text" id="lastName" placeholder="Last Name" className="mb-3" ref={(c) => lastName = c}/>
+                    </div>
                      
-                        <input
-                            type="text"
-                            id="username"
-                            placeholder="Username"
-                            className="mb-3"
-                            ref={(c) => registerLogin = c}
-                            autoComplete="off"
-                            onChange={(e) => setUser(e.target.value)}
-                            value={user}
-                            required
-                            aria-invalid={validName ? "false" : "true"}
-                            aria-describedby="uidnote"
-                            onFocus={() => setUserFocus(true)}
-                            onBlur={() => setUserFocus(false)}
-                        />
-                        <p id="uidnote" className={userFocus && user && !validName ? "instructions" : "offscreen"}>
-                            <FontAwesomeIcon icon={faInfoCircle} />
-                            4 to 24 characters.<br />
-                            Must begin with a letter.<br />
-                            Letters, numbers, underscores, hyphens allowed.
-                        </p>
+                    <input type="text" id="username" placeholder="Username" className="mb-3" ref={(c) => registerLogin = c}/>
+                    <input type="text" id="email" placeholder="Email" className="mb-3" ref={(c) => registerEmail = c}/>
+                    <input type="password" id="password" placeholder="Password" className="mb-3" ref={(c) => registerPassword = c}/>
+                    <input type="password" id="confirm_pwd" placeholder="Confirm password" ref={(c) => confirmPassword = c}/>
 
-                     
-                        <input
-                       type="text"  
-                       id="email"  
-                       placeholder="Email"
-                       className="mb-3"
-                       ref={(c) => registerEmail = c}/>
-                      
-
-                        <input
-                            type="password"
-                            id="password"
-                            placeholder="Password"
-                            className="mb-3"
-                            ref={(c) => registerPassword = c}
-                            onChange={(e) => setPwd(e.target.value)}
-                            value={pwd}
-                            required
-                            aria-invalid={validPwd ? "false" : "true"}
-                            aria-describedby="pwdnote"
-                            onFocus={() => setPwdFocus(true)}
-                            onBlur={() => setPwdFocus(false)}
-                        />
-                        <p id="pwdnote" className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
-                            <FontAwesomeIcon icon={faInfoCircle} />
-                            8 to 24 characters.<br />
-                            Must include uppercase and lowercase letters, a number and a special character.<br />
-                            Allowed special characters: <span aria-label="exclamation mark">!</span> <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span> <span aria-label="dollar sign">$</span> <span aria-label="percent">%</span>
-                        </p>
-
-                        <input
-                            type="password"
-                            id="confirm_pwd"
-                            placeholder="Confirm password"
-                            ref={(c) => confirmPassword = c}
-                            onChange={(e) => setMatchPwd(e.target.value)}
-                            value={matchPwd}
-                            required
-                            aria-invalid={validMatch ? "false" : "true"}
-                            
-                            aria-describedby="confirmnote"
-                            onFocus={() => setMatchFocus(true)}
-                            onBlur={() => setMatchFocus(false)}
-                        />
-                        <p id="confirmnote" className={matchFocus && !validMatch ? "instructions" : "offscreen"}>
-                            <FontAwesomeIcon icon={faInfoCircle} />
-                            Must match the first password input field.
-                        </p>
-
-                        <button className="mt-3" disabled={!validName || !validPwd || !validMatch ? true : false} onSubmit={handleSubmit}>Sign Up</button>
-                    </form>
+                    <button className="mt-3" onSubmit={doRegister}>Sign Up</button>
+                </form>
                     
-                    <p>
+                <p>
                     <span className="link text-center">
                         <Link to="/" variant = "body2">
                         <span style={{color :"black", fontSize: 15}}>
@@ -232,13 +111,12 @@ export function Register() {
                              Sign in
                          </span>
                         </Link>
-                        </span>
-                    </p>
+                    </span>
+                </p>
                      
                 </section>
             )}
-        </>
-    )
-}
+    
+
 
 export default Register
