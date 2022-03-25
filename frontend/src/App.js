e550e1a241c0dfcd4f1a51171b540184012eced8
Vrozzi23 <94210648+Vrozzi23@users.noreply.github.com>
2022-03-25 03:04:43 -0400

    Added my routes into App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,19 @@ import React from 'react';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import Messages from "./pages/Messages"
+import NotificationSettings from "./pages/NotificationSettings"
+import Switches from "./pages/Switches"
+import TriggerSettings from "./pages/TriggerSettings"
 
 function App() {
 
   return (
     <Router >
+      <style>
+          @import url('https://fonts.googleapis.com/css2?family=Nunito&display=swap');
+      </style>
             <Switch>
                 <Route path="/Login" exact>
                     <LoginPage />
@@ -15,7 +23,14 @@ function App() {
                     <RegisterPage />
                 </Route>
                 <Redirect to="/Login" />
+                <Route path='/' exact component={Homepage} /> 
+                <Route path='/Switches' exact component={Switches} />
+                <Route path='/TriggerSettings' component={TriggerSettings} />
+                <Route path='/Messages' component={Messages} />
+                <Route path='/NotificationSettings' component={NotificationSettings} />
             </Switch>  
+
+         
         </Router>
   );
 }
