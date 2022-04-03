f70fa79e8b2bdf2bdeccf12c9e44f4e7b16be6e9
Gavin <97692441+Ramphase@users.noreply.github.com>
2022-04-02 22:03:07 -0400

    Add files via upload

---
+++ models/Contacts.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+//Create Schema
+const ContactsSchema = new Schema({
+  UserId: {
+    type: Number,
+    required: true
+  },
+  FirstName: {
+    type: String,
+    required: true
+  },
+  LastName: {
+    type: String,
+    required: false
+  },
+  Email: {
+    type: String,
+    required: true
+  },
+  PhoneNumber: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = Contacts = mongoose.model("Contacts", ContactsSchema, "Contacts");
---
+++ models/Triggers.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+//Create Schema
+const TriggersSchema = new Schema({
+  UserId: {
+    type: Number,
+    required: true
+  },
+  TriggerName: {
+    type: String,
+    required: true
+  },
+  Message: {
+    type: String,
+    required: true
+  },
+  Contact: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = Triggers = mongoose.model("Triggers", TriggersSchema, "Triggers");
