e0f6653c4865252029d74e59bcfc5f29503e9cfa
Vrozzi23 <94210648+Vrozzi23@users.noreply.github.com>
2022-04-11 19:55:39 -0400

    Updated Switches Page

--- frontend/src/pages/SwitchesPage.js
+++ frontend/src/pages/SwitchesPage.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Navbar from '../components/navbar/Navbar'
 import Switches from '../components/Switches';
 
+
 const SwitchesPage = () => {
 return (
     <div>
         <Navbar/>
         <Switches/>
+        
     </div> 
     );
 };
