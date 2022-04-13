196fde09dd3856c3598fcd5da74a736832df5a09
Gavin <97692441+Ramphase@users.noreply.github.com>
2022-04-12 23:10:20 -0400

    Update Contacts.js

--- models/Contacts.js
+++ models/Contacts.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 //Create Schema
 const ContactsSchema = new Schema({
+  ContactId: {
+    type: Number
+  },
   UserId: {
     type: Number,
     required: true
